15074224f10e04f8ad9635d671d03f52faad32e2
Kthendavid04 <kthendavid04@gmail.com>
2021-08-05 15:22:54 -0500

    created schema folder and files

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
+//added books
+const Book = require('./Book');
 
-module.exports = { User };
+module.exports = { User, Book };
--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+// added Apollo server
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+//
 const app = express();
 const PORT = process.env.PORT || 3001;
-// added Apollo server
+// 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
