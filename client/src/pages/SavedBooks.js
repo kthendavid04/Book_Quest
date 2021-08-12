63e28ff83dfb46f7394617c6e53c494892672505
Kthendavid04 <kthendavid04@gmail.com>
2021-08-12 01:31:15 -0500

    updated user login and signup froms to utilize mutations instead of API's

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { getMe } from '../utils/queries';
+import { deleteBook } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
