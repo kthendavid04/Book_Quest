3a7b0e4be5c069931bb2f539f7bb9b9ab2345c69
Kthendavid04 <kthendavid04@gmail.com>
2021-08-13 11:26:30 -0500

    updated middleware

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
@@ -20,7 +19,6 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
