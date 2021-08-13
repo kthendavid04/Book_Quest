e4ca4388cd29c9a319fee7d89f761753c561a1d0
Kthendavid04 <kthendavid04@gmail.com>
2021-08-12 22:21:02 -0500

    updated file path in earchbook.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook } from '../utils/mutation';
-import {searchGoogleBooks} from ',,/utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { saveBook } from '../utils/mutations';
+import {searchGoogleBooks} from '../utils/API';
+import { saveBookIds, getSavedBookIds } from '../utils/mutations.js';
 
 
 const SearchBooks = () => {
